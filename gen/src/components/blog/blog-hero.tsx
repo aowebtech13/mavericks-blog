f2dc99756a695e10e5d18d35993bb93351b2e8ce
aowebtech13 <adesokanadedeji10@gmail.com>
2026-09-14 08:57:14 -0700

    at

--- gen/src/components/blog/blog-hero.tsx
+++ gen/src/components/blog/blog-hero.tsx
@@ -19,8 +19,7 @@ const BlogHero = () => {
           </TextReveal>
           <TextReveal delay={0.2}>
             <p className="text-tagline-2 mx-auto mt-3 mb-14 max-w-xl font-normal text-white/60">
-              At MavericksAI we share practical insights and real-world tips to help you make the most
-              of AI in your business.
+            At MavericksAI, we share practical insights and helpful tips about legal process and procedures around the world
             </p>
           </TextReveal>
         </div>
